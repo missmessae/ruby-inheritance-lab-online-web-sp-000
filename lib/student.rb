ea7410f20c0486483dd6745ab9f8f3672a4c7ec7
Annie Wiegel <annewiegel17@gmail.com>
2021-02-19 00:08:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,10 +12,4 @@ class Student < User
     @knowledge
   end
 
-
-  Define a method, #learn, that takes in a string and adds
-  that string to the student's @knowledge array.
-  Define a method, #knowledge, that returns that student's knowledge array.
-
-
 end

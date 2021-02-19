a3a7e4d85f4ee31f678124537d6ca8c06098fff9
Annie Wiegel <annewiegel17@gmail.com>
2021-02-19 00:08:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,12 @@ class Student < User
     @knowledge = []
   end
 
+  def learn(fact)
+    @knowledge << fact
 
-  Individual students should initialize with an i
-  nstance variable, @knowledge, that points to an empty array.
-  Define a method, #learn, that takes in a string and adds that string to the student's @knowledge array.
+
+  Define a method, #learn, that takes in a string and adds
+  that string to the student's @knowledge array.
   Define a method, #knowledge, that returns that student's knowledge array.
 
 

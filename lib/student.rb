128726c3966700ac87124f1c54414968f0c75085
Annie Wiegel <annewiegel17@gmail.com>
2021-02-19 00:07:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
 class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+
+  Individual students should initialize with an i
+  nstance variable, @knowledge, that points to an empty array.
+  Define a method, #learn, that takes in a string and adds that string to the student's @knowledge array.
+  Define a method, #knowledge, that returns that student's knowledge array.
+
+
 end
